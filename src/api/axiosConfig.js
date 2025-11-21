5b845b12a7627bf8882ed1c40368b583990fa30b
Abhinav <abhinavsd07@gmail.com>
2025-11-21 13:17:18 +0530

    Movie_booking

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://movie-booking-server-1.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
